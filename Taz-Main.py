3163f2c447a48a89b133ee9924cb71c60492bd97
YarPhys <iarenkov.av@phystech.edu>
2021-11-24 19:31:00 +0300

    Fixed playing music in menu after game_over screen

--- Taz-Main.py
+++ Taz-Main.py
@@ -289,7 +289,7 @@ def game_over(bg, screen):
     :param screen:
     :return:
     """
-    global is_menu, running_1, is_game_over
+    global is_menu, running_1, is_game_over, menu_music
     pygame.mixer.music.load('Music/game_over.mp3')
     pygame.mixer.music.play()
     screen.fill(Color(WHITE))
@@ -321,10 +321,11 @@ def game_over(bg, screen):
                     run = False
                 if menu_button.is_pressed(mouse_pos, mouse_pressed):
                     is_menu = True
+                    menu_music = False
                     is_game_over = False
                     run = False
     pygame.display.update()
-    return is_menu, running_1, is_game_over
+    return is_menu, running_1, is_game_over,  menu_music
 
 
 def level_1(bg, screen):
